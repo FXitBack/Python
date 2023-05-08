058d21e606b4e65eb46d7f548bb23f9326237595
FXitBack <FXitBack@gmail.com>
2023-05-08 16:25:30 +0300

    Creating variables

--- Python.py
+++ Python.py
@@ -1,1 +1,23 @@
-#
+# print (wsup)
+# **- Возведение в степень
+# %- Деление по модулю
+# // - целочисленное деление с отбрасыванием дробной части
+print(27 % 3)
+print(27 // 3)
+print(27 + 3)
+print(27 - 3)
+print(27 * 3)
+# int() - целочисленый тип данных, целые числа
+# "" -  str() строки
+# float - вещественные числа с плавающей точкой
+my_name = "Vladimir "
+my_secondname = "Ivanov"
+my_age = 29
+print(my_name +" "+ my_secondname)
+print(my_name +" "+ str(my_age) +" "+ my_secondname)
+print(f"{my_name} мой возраст {my_age}")
+print(f"{my_name} {my_name} {my_name} {my_name} {my_name}")
+print(f"{my_name*5 }")
+# Переменная - Это область памяти компьютера, в которой может хранится одиночное значение, знак = является оператором присваивания  и сохроняет значение
+color = "green"
+print(color)
