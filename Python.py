0215609d2cd53e54123c5c1c0a9907566a1508c7
FXitBack <FXitBack@gmail.com>
2023-05-08 20:46:42 +0300

    len+round

--- Python.py
+++ Python.py
@@ -1,23 +1,48 @@
-# print (wsup)
-# **- Возведение в степень
-# %- Деление по модулю
-# // - целочисленное деление с отбрасыванием дробной части
-print(27 % 3)
-print(27 // 3)
-print(27 + 3)
-print(27 - 3)
-print(27 * 3)
-# int() - целочисленый тип данных, целые числа
-# "" -  str() строки
-# float - вещественные числа с плавающей точкой
-my_name = "Vladimir "
-my_secondname = "Ivanov"
-my_age = 29
-print(my_name +" "+ my_secondname)
-print(my_name +" "+ str(my_age) +" "+ my_secondname)
-print(f"{my_name} мой возраст {my_age}")
-print(f"{my_name} {my_name} {my_name} {my_name} {my_name}")
-print(f"{my_name*5 }")
-# Переменная - Это область памяти компьютера, в которой может хранится одиночное значение, знак = является оператором присваивания  и сохроняет значение
-color = "green"
-print(color)
+# # print (wsup)
+# # **- Возведение в степень
+# # %- Деление по модулю
+# # // - целочисленное деление с отбрасыванием дробной части
+# print(27 % 3)
+# print(27 // 3)
+# print(27 + 3)
+# print(27 - 3)
+# print(27 * 3)
+# # int() - целочисленый тип данных, (целые числа)
+# # "" -  str() строки
+# # float - вещественные числа (с плавающей точкой)
+# my_name = "Vladimir "
+# my_secondname = "Ivanov"
+# my_age = 29
+# print(my_name +" "+ my_secondname)
+# print(my_name +" "+ str(my_age) +" "+ my_secondname)
+# print(f"{my_name} мой возраст {my_age}") # f"{Переменная}строка" - функция f-string, которая делает интерполяцию
+# print(f"{my_name} {my_name} {my_name} {my_name} {my_name}")
+# print(f"{my_name*5 }")
+# # Переменная - Это область памяти компьютера, в которой может хранится одиночное значение, знак = является оператором присваивания  и сохроняет значение
+# color = "green"
+# print(color)
+# # Имя переменной должно представлять собой одно слово без пробелов, в имени переменной могут использоваться буквы, цифры, символы сподчеркивание
+# # Имя переменной не должно начинатся с цифры!
+# _ = "My little var"
+# print(_)
+# print(len(my_name)) # 8 letters+backspace
+# # my_name = int(input("Имя1:"))
+# my_name1 = float(input("Имя2:"))
+# print(5 + my_name1)
+# # my_age = input("Возраст:")
+# # print("Ты еще молод")
+# #Все что вводится в input на выходе будет строка
+# # Конкатенация - это сложение/склеивание строк (my_name + my_name)
+# # Реплекация - умножение разных типов данных (строка на число 5*my_name)
+# # len() - относиться к категории встроенных фун-ий
+
+# math = int(input("Ввод:"))
+# math2 = float(input("Ввод:"))
+# print(math % 3 *math2)
+
+# math = float(input("Ввод:"))
+# math1 = float(input("Ввод:"))
+# print(round (math * math1))
+
+alpabet = (input ("Ввод:"))
+print(len(alpabet))
