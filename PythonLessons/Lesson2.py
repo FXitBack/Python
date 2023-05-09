649c93ed0808dc5c30b271b0fbd619937ba87601
FXitBack <FXitBack@gmail.com>
2023-05-09 20:39:45 +0300

    Lesson3

---
+++ PythonLessons/Lesson2.py
@@ -0,0 +1,25 @@
+# number = 5  # INT
+# print("Результат", number)
+#
+# number = 7
+# print("Результат", number)
+# digit = 4.3333333  # FLOAT
+# word = 'Результат '  # STRING
+# str_num = '5'  # STRING
+# print(number + int(str_num))
+# print(word, digit)
+#
+# boolean = True or False  # BOOL
+# print(word + str(digit))
+
+
+num1 = int(input('Введите первое число: '))   #string => int
+num2 = int(input('Введите второе число: '))  #string => int
+
+print("Result: ", num1 / num2)
+print("Result: ", num1 * num2)
+print("Result: ", num1 + num2)
+print("Result: ", num1 - num2)
+print("Result: ", num1 % num2)
+print("Result: ", num1 ** num2)
+print("Result: ", num1 // num2)
